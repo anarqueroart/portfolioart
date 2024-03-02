f6f3a312877a34fffb982533619b72fe1f4962b0
SKIQQ <119549330+SKIQQ-flower@users.noreply.github.com>
2024-03-02 12:05:51 +0000

    [WIP] (Starting up) Empty website with a titlebar

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <Link rel="preconnect" href="https://fonts.googleapis.com"/>
+  <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+  <Link href="https://fonts.googleapis.com/css2?family=Lobster&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
+  <div>
+    <titlebar/>
+  </div>
+</template>
---
+++ components/titlebar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="titlebar flex-row items-center justify-between flex w-full h-fit gap-2">
+        <div class="flex flex-row">  
+          <img class="size-14 rounded-full pfp-shadow" src="/pfp.jpg">
+          <img class="w-48" src="/asset_logo.svg">
+        </div>
+        <button class="bg-[#FF93AA] p-2 rounded-lg">
+          Source Code
+        </button>
+    </div>
+</template>
+
+<style scoped>
+  .titlebar {
+    border-radius: 0px 0px 30px 30px;
+    background-color: rgba(255, 204, 230, 1);
+    box-shadow: 2px 2px 0px 0px rgba(255, 147, 170, 1);
+    padding: 5px 25px 5px 25px;
+  }
+
+  .pfp-shadow {
+    box-shadow: 1px 1px 0px 0px rgba(255, 147, 170, 1);
+  }
+</style>
